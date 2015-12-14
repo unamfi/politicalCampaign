0f01652396572114c12d8211ffa112e525ff412c
saiphcita <saiphcita@gmail.com>
2015-12-13 20:43:02 -0500

    obtaining post info

--- readDataSQL.py
+++ readDataSQL.py
@@ -1,20 +1,46 @@
 #!/usr/bin/python
 import MySQLdb
+import operator
 
-db = MySQLdb.connect(host="localhost",    # your host, usually localhost
+def getPostDataFrom():
+	db = MySQLdb.connect(host="localhost",    # your host, usually localhost
                      user="root",         # your username
                      passwd="nathan",  # your password
                      db="politics")        # name of the data base
 
 
-# you must create a Cursor object. It will let
-#  you execute all the queries you need
-cur = db.cursor()
-# Use all the SQL you like
-cur.execute("SELECT * FROM post;")
-# print all the first cell of all the rows
-for row in cur.fetchall():
-   date=row[3]
-   if not date==None:
-   	print date
+	# you must create a Cursor object. It will let
+	#  you execute all the queries you need
+	cur = db.cursor()
+	# Use all the SQL you like
+	cur.execute("SELECT * FROM post;")
+	# print all the first cell of all the rows
+	dates={}
+	numPosts=0
+	for row in cur.fetchall():
+		#print row
+		
+		date=row[3]
+ 		if not date==None:
+ 			numPosts+=1
+			print date
+   			dates.setdefault(date,[])
+   			
+			postText=row[11]
+			textoComplete=row[12]
+			#if "video" in url:
+			#print "ES VIDEO"
+			url=row[10]
+			print postText
+			print textoComplete
+			print url
+   			#break
+   	print numPosts
+
+
+getPostDataFrom()
+#sorted_dates = sorted(dates.items(), key=operator.itemgetter(0),reverse=True)
+#for d,v in sorted_dates:
+#	print d
+
     #break
